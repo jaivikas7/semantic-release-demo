587890ef110f5c17dde48a65078085e75a3cbf26
jaivikas7 <44570847+jaivikas7@users.noreply.github.com>
2022-02-27 04:08:39 +0530

    fix: dummy

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Vikku!');
+  res.end('Hello Viku!');
 });
 
 server.listen(port, hostname, () => {
