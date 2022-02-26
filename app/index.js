6a171f0b193fdfca9e6bc285b56332de5eeb4d36
Vikas Jayaraman <vikas.jayaraman@uipath.com>
2022-02-26 15:17:34 +0530

    fix: update response message to match the requirement

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello Vikas! You did a great job here.');
 });
 
 server.listen(port, hostname, () => {
